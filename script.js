30090c3eaeaa780375752da82634475d21c5d75f
Justin Maxwell <jm00724@my.westga.edu>
2022-10-15 19:04:34 -0400

    Script added

---
+++ script.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    if (document.forms[0].myEmail.value == "") {
+        alert("Please enter an e-mail address.");
+        return false;
+    } // end if
+    return true;
+} // end function validateForm
